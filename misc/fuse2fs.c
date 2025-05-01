7a4e466bb0bc514cc7c9092c50a76bfd5799df42
Darrick J. Wong <djwong@kernel.org>
2025-05-01 14:00:09 -0700

    fuse2fs: recheck support after replaying journal
    The journal could have contained a new primary superblock, so we need to
    recheck feature support after recovering it.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -1036,6 +1036,10 @@ static errcode_t fuse2fs_mount(struct fuse2fs *ff)
 			}
 			ext2fs_clear_feature_journal_needs_recovery(fs->super);
 			ext2fs_mark_super_dirty(fs);
+
+			err = fuse2fs_check_support(ff);
+			if (err)
+				return err;
 		}
 	}
 
