3cc8b7cb39d92e15d16dd287059239eb7577dbb5
Darrick J. Wong <djwong@kernel.org>
2025-05-01 12:24:21 -0700

    fuse2fs: register as an IO flusher thread
    fuse2fs is involved in the filesystem I/O path and can allocate memory
    while processing I/O requests.  Therefore, we need to register that fact
    with the kernel so that memory allocations done by libext2fs don't start
    a round of filesystem memory reclaim, which could cause more writeout to
    get dumped on fuse2fs.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -17,6 +17,7 @@
 # include <linux/fs.h>
 # include <linux/falloc.h>
 # include <linux/xattr.h>
+# include <sys/prctl.h>
 #endif
 #ifdef HAVE_SYS_XATTR_H
 #include <sys/xattr.h>
@@ -4950,6 +4951,20 @@ int main(int argc, char *argv[])
 			stdout = fp;
 		}
 	}
+
+#ifdef HAVE_PR_SET_IO_FLUSHER
+	/*
+	 * Register as a filesystem I/O server process so that our memory
+	 * allocations don't cause fs reclaim.
+	 */
+	ret = prctl(PR_SET_IO_FLUSHER, 1, 0, 0, 0);
+	if (ret < 0) {
+		err_printf(&fctx, "%s: %s.\n",
+ _("Could not register as IO flusher thread"),
+				strerror(errno));
+		ret = 0;
+	}
+#endif
 
 	/* Will we allow users to allocate every last block? */
 	if (getenv("FUSE2FS_ALLOC_ALL_BLOCKS")) {
