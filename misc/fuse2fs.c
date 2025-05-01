99515b608c6b71af26bffdf58432a47c4deff42a
Darrick J. Wong <djwong@kernel.org>
2025-05-01 14:48:17 -0700

    fuse2fs: track our own writable state
    Track our own willingness to write to the filesystem in a separate
    variable from that of libext2fs.  There's a small window between opening
    the filesystem and mounting it where the library is rw but we might fail
    a mount task and therefore don't want to write anything more.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -208,6 +208,7 @@ struct fuse2fs {
 	uint8_t directio;
 	uint8_t acl;
 	uint8_t dirsync;
+	uint8_t writable;
 
 	int blocklog;
 	unsigned int blockmask;
@@ -480,8 +481,6 @@ static int update_atime(ext2_filsys fs, ext2_ino_t ino)
 	struct timespec atime, mtime, now;
 	double datime, dmtime, dnow;
 
-	if (!(fs->flags & EXT2_FLAG_RW))
-		return 0;
 	err = fuse2fs_read_inode(fs, ino, &inode);
 	if (err)
 		return translate_error(fs, ino, err);
@@ -597,9 +596,7 @@ static int fs_can_allocate(struct fuse2fs *ff, blk64_t num)
 
 static int fs_writeable(struct fuse2fs *ff)
 {
-	ext2_filsys fs = ff->fs;
-
-	return (fs->flags & EXT2_FLAG_RW) && (fs->super->s_error_count == 0);
+	return ff->writable && (ff->fs->super->s_error_count == 0);
 }
 
 static inline int is_superuser(struct fuse2fs *ff, struct fuse_context *ctxt)
@@ -796,6 +793,7 @@ static errcode_t open_fs(struct fuse2fs *ff, int libext2_flags)
 	}
 
 	snprintf(options, sizeof(options) - 1, "offset=%lu", ff->offset);
+	ff->writable = 0;
 
 	if (ff->directio)
 		flags |= EXT2_FLAG_DIRECT_IO;
@@ -947,6 +945,7 @@ _("Mounting read-only without recovering journal."));
 			translate_error(fs, 0, err);
 			return err;
 		}
+		ff->writable = 1;
 	}
 
 	if (!(fs->super->s_state & EXT2_VALID_FS))
@@ -990,7 +989,7 @@ static void op_destroy(void *p EXT2FS_ATTR((unused)))
 	fs = ff->fs;
 
 	dbg_printf(ff, "%s: dev=%s\n", __func__, fs->device_name);
-	if (fs->flags & EXT2_FLAG_RW) {
+	if (ff->writable) {
 		fs->super->s_state |= EXT2_VALID_FS;
 		if (fs->super->s_error_count)
 			fs->super->s_state |= EXT2_ERROR_FS;
@@ -1099,7 +1098,7 @@ static void *op_init(struct fuse_conn_info *conn
 	}
 
 	/* Clear the valid flag so that an unclean shutdown forces a fsck */
-	if (fs->flags & EXT2_FLAG_RW) {
+	if (ff->writable) {
 		fs->super->s_mnt_count++;
 		ext2fs_set_tstamp(fs->super, s_mtime, time(NULL));
 		fs->super->s_state &= ~EXT2_VALID_FS;
@@ -3229,7 +3228,7 @@ static int op_statfs(const char *path EXT2FS_ATTR((unused)),
 	fsid ^= *f;
 	buf->f_fsid = fsid;
 	buf->f_flag = 0;
-	if (fs->flags & EXT2_FLAG_RW)
+	if (ff->writable)
 		buf->f_flag |= ST_RDONLY;
 	buf->f_namemax = EXT2_NAME_LEN;
 
@@ -5345,6 +5344,7 @@ static int __translate_error(ext2_filsys fs, ext2_ino_t ino, errcode_t err,
 		err_printf(ff, "%s\n",
  _("Remounting read-only due to errors."));
 		fs->flags &= ~EXT2_FLAG_RW;
+		ff->writable = 0;
 		break;
 	case EXT2_ERRORS_PANIC:
 		err_printf(ff, "%s\n",
