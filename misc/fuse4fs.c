ec060e4a6b0fad88949be57da9cb5cbb38ea70ec
Darrick J. Wong <djwong@kernel.org>
2025-05-19 14:36:50 -0700

    fuse2fs: don't zero bytes in punch hole
    When iomap is in use for the pagecache, it will take care of zeroing the
    unaligned parts of punched out regions so we don't have to do it
    ourselves.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- misc/fuse4fs.c
+++ misc/fuse4fs.c
@@ -5265,6 +5265,10 @@ static errcode_t fuse4fs_zero_middle(struct fuse4fs *ff, ext2_ino_t ino,
 	int retflags;
 	errcode_t err;
 
+	/* the kernel does this for us in iomap mode */
+	if (fuse4fs_iomap_enabled(ff))
+		return 0;
+
 	if (!*buf) {
 		err = ext2fs_get_mem(fs->blocksize, buf);
 		if (err)
@@ -5300,6 +5304,10 @@ static errcode_t fuse4fs_zero_edge(struct fuse4fs *ff, ext2_ino_t ino,
 	int retflags;
 	off_t residue;
 	errcode_t err;
+
+	/* the kernel does this for us in iomap mode */
+	if (fuse4fs_iomap_enabled(ff))
+		return 0;
 
 	residue = FUSE4FS_OFF_IN_FSB(ff, offset);
 	if (residue == 0)
