a1d81718c43652b9678ac8b9635cbc26451c0a05
Darrick J. Wong <djwong@kernel.org>
2025-08-28 10:32:08 -0700

    libsupport: add caching IO manager
    Start creating a caching IO manager so that we can have better caching
    of metadata blocks in fuse2fs.  For now it's just a passthrough cache.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

---
+++ lib/support/iocache.h
@@ -0,0 +1,17 @@
+/*
+ * iocache.h - IO cache
+ *
+ * Copyright (C) 2025-2026 Oracle.
+ *
+ * %Begin-Header%
+ * This file may be redistributed under the terms of the GNU Public
+ * License.
+ * %End-Header%
+ */
+#ifndef __IOCACHE_H__
+#define __IOCACHE_H__
+
+errcode_t iocache_set_backing_manager(io_manager manager);
+extern io_manager iocache_io_manager;
+
+#endif /* __IOCACHE_H__ */
