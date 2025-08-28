769aa3e0636e46bf094cc7231a629ee699ff3f84
Darrick J. Wong <djwong@kernel.org>
2025-08-28 10:30:50 -0700

    libext2fs: make it possible to extract the fd from an IO manager
    Make it so that we can extract the fd from an open IO manager.  This
    will be used in subsequent patches to register the open block device
    with the fuse iomap kernel driver.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- lib/ext2fs/ext2_io.h
+++ lib/ext2fs/ext2_io.h
@@ -103,7 +103,8 @@ struct struct_io_manager {
 	errcode_t (*zeroout)(io_channel channel, unsigned long long block,
 			     unsigned long long count);
 	errcode_t (*flock)(io_channel channel, unsigned int flock_flags);
-	long	reserved[13];
+	errcode_t (*get_fd)(io_channel channel, int *fd);
+	long	reserved[12];
 };
 
 #define IO_FLAG_RW		0x0001
@@ -155,6 +156,7 @@ extern errcode_t io_channel_cache_readahead(io_channel io,
 					    unsigned long long count);
 extern errcode_t io_channel_flock(io_channel io, unsigned int flock_flags);
 extern errcode_t io_channel_funlock(io_channel io);
+extern errcode_t io_channel_get_fd(io_channel io, int *fd);
 
 #ifdef _WIN32
 /* windows_io.c */
