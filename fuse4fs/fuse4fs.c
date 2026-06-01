2eefa83583b8c0c67631964aa6670a0fb065dd68
Darrick J. Wong <djwong@kernel.org>
2026-06-01 16:52:30 -0700

    fuse2fs: increase inode cache size
    Increase the internal inode cache size.  Does this improve performance
    any?
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fuse4fs/fuse4fs.c
+++ fuse4fs/fuse4fs.c
@@ -1689,6 +1689,10 @@ static errcode_t fuse4fs_open(struct fuse4fs *ff)
 	if (err)
 		return translate_error(ff->fs, 0, err);
 
+	err = ext2fs_create_inode_cache(ff->fs, 1024);
+	if (err)
+		return translate_error(ff->fs, 0, err);
+
 	ff->fs->priv_data = ff;
 	ff->blocklog = u_log2(ff->fs->blocksize);
 	ff->blockmask = ff->fs->blocksize - 1;
